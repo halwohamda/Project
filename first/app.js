ab6f5d7af3293496b335d5fe625d93d9f0f9e4c8
halwohamda <halwohamda@gmail.com>
2024-03-04 11:39:38 -0800

    done

--- first/app.js
+++ first/app.js
@@ -10,6 +10,6 @@ menu.onclick = () =>{
     navbar.classList.toggle('open');
 };
 window.onscroll = () =>{
-    menu.classList.remove('bx-x');
+    // menu.classList.remove('bx-x');
     // navbar.classList.remove('open');
 }
