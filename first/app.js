0e236d8b26aa065f6713b0ade1739833bec33b1b
halwohamda <halwohamda@gmail.com>
2024-03-04 11:41:35 -0800

    done

--- first/app.js
+++ first/app.js
@@ -2,7 +2,7 @@ const header = document.querySelector("header");
 window.addEventListener("scroll",function(){
     header.classList.toggle("sticky",this.window.scrollY >90)
 })
-// let menu = document.querySelector('#menu-icon')
+let menu = document.querySelector('#menu-icon')
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () =>{
